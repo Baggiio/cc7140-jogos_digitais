008d4def34e88c2b75cba44f15f1db5f6efee2f1
Ruan <turolaruan2@gmail.com>
2025-02-20 23:51:58 -0300

    obstaculos adicionados

---
+++ AirHockey/Assets/structure.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class structure : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public int health = 100;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+        UpdateColor();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void UpdateColor()
+    {
+        float healthPercentage = (float)health / 100;
+        spriteRenderer.color = new Color(1, healthPercentage, healthPercentage);
+    }
+}
