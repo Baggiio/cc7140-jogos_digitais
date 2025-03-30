629964230aa6ad6c3933ef3dcd9a67acebb6caa3
Ruan <turolaruan2@gmail.com>
2025-03-30 16:58:26 -0300

    Jogo Plataforma

---
+++ Plataforma/Assets/Block.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject gameManager;
+    public Sprite spriteImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Display");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        // change sprite when health is less than 50
+        else if (health <= 50)
+        {
+            SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = spriteImage;
+        }
+    }
+}
